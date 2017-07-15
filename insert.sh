92c1beb877fdbf2ec6e361aa6e136b192edd54de
Tomoya Tabuchi <t@tomoyat1.com>
2017-07-15 16:05:31 +0900

    Add index template to make kuromoji_tokenizer default

--- insert.sh
+++ insert.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-curl -XPUT 'http://localhost:9200/_all/_settings?preserve_existing=true' -d '{
-  "index.analysis.analyzer.default.tokenizer" : "kuromoji_tokenizer",
-  "index.analysis.analyzer.default.type" : "custom"
+curl --user elastic:changeme -XPUT 'http://localhost:9200/_template/kuromoji_tokenizer_default' -d '{
+    "template" : "*",
+    "settings" : {
+        "number_of_shards" : 1,
+        "index": {
+            "analysis": {
+                "analyzer": {
+                    "default": {
+                        "tokenizer": "kuromoji_tokenizer",
+                        "type": "custom"
+                    }
+                }
+            }
+        }
+    }
 }'
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-ls -al /utils
-#/bin/bash /utils/wait-for-it.sh -t 0 localhost:9200 -- ./insert.sh &
+/bin/bash /utils/wait-for-it.sh -t 0 localhost:9200 -- ./insert.sh &
 elasticsearch
